7b4b269821c8736d2756bb2501540914ca9652ff
chris <abc@abchris.xyz>
2021-07-19 13:09:10 +1200

    Make logo a link

--- ewi/src/App.js
+++ ewi/src/App.js
@@ -58,7 +58,7 @@ function App() {
         <div className={showNav ? "wrapper toggled" : "wrapper"}>
           <header>
             <div className="logo">
-              ErgoWatch
+              <a href="/">ErgoWatch</a>
               <button id="nav-burger" onClick={() => setShowNav(!showNav)}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 16 16">
                   <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
