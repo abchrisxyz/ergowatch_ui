d37908fbfe7ec77778b71332c96d4e115fd43b91
chris <abc@abchris.xyz>
2021-12-03 12:22:12 +1300

    whitespace

--- ewi/src/pages/metrics/utxos.js
+++ ewi/src/pages/metrics/utxos.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+
 import { AddressLink } from "../../components/links";
 import Card from "../../components/card";
 import colors from "../../config/colors";
